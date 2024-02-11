1749d4bcbff910286df0027c6302ff932ab976ed
joserigel <joserigel18@gmail.com>
2024-02-11 23:37:40 +0100

    Fixed DataReader.py

--- DataReader.py
+++ DataReader.py
@@ -12,20 +12,22 @@ async def connect(collection):
     uri = f"mongodb://{os.environ.get('MONGO_USER')}:{os.environ.get('MONGO_PASS')}@localhost:27017"
     client = AsyncIOMotorClient(uri)
     db = client['bdd100k']
-    return db['sem_seg_polygons_val']
+    return db[collection]
 
-async def loadIntoMongoDB(file, collection):
+async def loadIntoMongoDB(file: str, collection):
+    print(f"Copying {file.split('/')[-1]} into {collection}")
     with open(file) as f:
         datas = json.load(f)
         db_collection = await connect(collection)
         for data in tqdm(datas):
             await db_collection.insert_one(data)
-        print(f"Finished copying {file.split("/")[-1]} into {collection}")
+        print(f"Finished copying {file.split('/')[-1]} into {collection}")
 
 async def main():
-    print("Loading!")
-    await loadIntoMongoDB('./bdd100k/labels/sem_seg/polygons/sem_seg_train.json', 'sem_seg_polygons')
-    await loadIntoMongoDB('./bdd100k/labels/sem_seg/polygons/sem_seg_val.json', 'sem_seg_polygons_val')
+    await loadIntoMongoDB(f'{os.environ.get("BDD100K_DIR")}/labels/sem_seg/polygons/sem_seg_train.json', 'sem_seg_polygons')
+    await loadIntoMongoDB(f'{os.environ.get("BDD100K_DIR")}/labels/sem_seg/polygons/sem_seg_val.json', 'sem_seg_polygons_val')
+    await loadIntoMongoDB(f'{os.environ.get("BDD100K_DIR")}/labels/lane/polygons/lane_train.json', 'lane_polygons')
+    await loadIntoMongoDB(f'{os.environ.get("BDD100K_DIR")}/labels/lane/polygons/lane_val.json', 'lane_polygons_val')
 
 asyncio.run(main())
 
