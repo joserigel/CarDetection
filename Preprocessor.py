f97fd2401ce8910b5fb96e0fb709ddab397bc430
joserigel <joserigel18@gmail.com>
2024-08-04 13:18:27 +0200

    Optimized for CUDA

--- Preprocessor.py
+++ Preprocessor.py
@@ -26,8 +26,12 @@ async def preprocess(image):
     mid = 0.5
     mean = np.mean(gray)
     gamma = np.log(mid*255)/np.log(mean)
-    img_gamma1 = np.power(image, gamma).clip(0,255).astype(np.uint8)
-    return img_gamma1
+    corrected = np.power(image, gamma).clip(0,255).astype(np.uint8) #/ 255
+    # edges = cv2.Canny(image, 100, 200) / 255
+    # corrected[:, :, 0] *= edges
+    # corrected[:, :, 1] *= edges
+    # corrected[:, :, 2] *= edges
+    return corrected #* 255
 directory = os.listdir("bdd100k/images/10k/train/")
 async def filter():
     dbConnection = await connect("det")
@@ -51,7 +55,7 @@ async def getBatch(size, resolution=(240, 427), preprocessed=True):
     dbConnection = await connect("sem_seg_polygons")
     
     inputs = np.zeros((size, 3, 1280, 720), dtype=np.float32)
-    labels = np.zeros((size, 1, resolution[1], resolution[0]), dtype=np.float32)
+    labels = np.zeros((size, 2, resolution[1], resolution[0]), dtype=np.float32)
     
     sample = random.sample(directory, size)
     
@@ -66,64 +70,62 @@ async def getBatch(size, resolution=(240, 427), preprocessed=True):
 
         mask = np.zeros((input_image.shape[2], input_image.shape[1]))
         for label in data["labels"]:
-            if label["category"] == "road":
+            if label["category"] in ["car", "truck", "bus"]:
                 for polygon in label["poly2d"]:
                     pts = np.array([polygon["vertices"]])
                     pts.reshape((-1, 1, 2))
 
                     cv2.fillPoly(mask, pts.astype(dtype=np.int32), color=(255, 255, 255))
                     
-        mask = cv2.resize(mask, (resolution[0], resolution[1]), interpolation=cv2.INTER_NEAREST)
+        mask = cv2.resize(mask, (resolution[0], resolution[1]), interpolation=cv2.INTER_AREA)
         inverted = 255 - mask
 
         
-        labels[i] = [mask / 255]
+        labels[i] = [mask / 255, inverted / 255]
 
 
     return inputs / 255, labels
 
-global counter
-counter = 0
-async def loadToMemory(count, resolution=(240, 427), preprocessed=True):
-    global counter
-    if counter >= count:
-        counter = 0
-
+directory = samples
+async def saveToStorage(count, resolution=(240, 427), preprocessed=True):
     size = len(directory) // count
-
-    actual_size = min(size, len(directory) - (size * counter))
-
-
-    inputs = np.zeros((actual_size, 3, 1280, 720), dtype=np.float32)
-    labels = np.zeros((actual_size, 1, resolution[1], resolution[0]), dtype=np.float32)
-
     dbConnection = await connect("sem_seg_polygons")
-    for i, image in enumerate(tqdm(directory[size * counter: (size * counter) + size])):
-        input_image = cv2.imread(f"bdd100k/images/10k/train/{image}")
-        if preprocessed:
-            input_image = await preprocess(input_image)
-        input_image = input_image.swapaxes(0, 2)
-        inputs[i] = input_image
-        data = await dbConnection.find_one({"name": image})
 
-        mask = np.zeros((input_image.shape[2], input_image.shape[1]))
-        for label in data["labels"]:
-            if label["category"] == "car":
-                for polygon in label["poly2d"]:
-                    pts = np.array([polygon["vertices"]])
-                    pts.reshape((-1, 1, 2))
+    for batch in range(count):
+        print("BATCH:", batch + 1, "/", count)
+        actual_size = min(size, len(directory) - (size * batch))
+        inputs = np.zeros((actual_size, 3, 1280, 720), dtype=np.float32)
+        labels = np.zeros((actual_size, 2, resolution[1], resolution[0]), dtype=np.float32)
 
-                    cv2.fillPoly(mask, pts.astype(dtype=np.int32), color=(255, 255, 255))
-                    
-        mask = cv2.resize(mask, (resolution[0], resolution[1]), interpolation=cv2.INTER_NEAREST)
-        inverted = 255 - mask
+        for i, image in enumerate(tqdm(directory[size * batch : (size * batch) + size])):
+            input_image = cv2.imread(f"bdd100k/images/10k/train/{image}")
+            if preprocessed:
+                input_image = await preprocess(input_image)
+            input_image = input_image.swapaxes(0, 2)
+            inputs[i] = input_image
+            data = await dbConnection.find_one({"name": image})
 
-        
-        labels[i] = [mask / 255]
+            mask = np.zeros((input_image.shape[2], input_image.shape[1]))
+            for label in data["labels"]:
+                if label["category"] in ["car", "truck", "bus"]:
+                    for polygon in label["poly2d"]:
+                        pts = np.array([polygon["vertices"]])
+                        pts.reshape((-1, 1, 2))
 
-    counter += 1
-    return inputs / 255, labels
+                        cv2.fillPoly(mask, pts.astype(dtype=np.int32), color=(255, 255, 255))
+                        
+            mask = cv2.resize(mask, (resolution[0], resolution[1]), interpolation=cv2.INTER_NEAREST)
+            inverted = 255 - mask
 
-# asyncio.run(loadToMemory(3))
+            
+            labels[i] = [mask / 255, inverted / 255]
+
+        with open(f'binaryDataset/inputs_{batch}.npy', 'wb') as f:
+            np.save(f, inputs / 255)
+
+        with open(f'binaryDataset/labels_{batch}.npy', 'wb') as f:
+            np.save(f, labels)
+
+# asyncio.run(saveToStorage(1, resolution=(64, 64)))
 
         
--- Segmentation.py
+++ Segmentation.py
@@ -22,148 +22,99 @@ import os
 load_dotenv()
 
 train_device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+train_device = 'cpu'
 print("====", "USING CUDA GPU" if train_device == 'cuda:0' else "USING CPU (SLOW)", "====")
 train_device = torch.device(train_device)
 
 class Segmentation(nn.Module):
     def __init__(self):
         super(Segmentation, self).__init__()
-        self.pooling = nn.Sequential(
-            nn.MaxPool2d((2, 2), (2, 2)),
-            nn.MaxPool2d((2, 2), (2, 2)),
-        )
         self.convolutional = nn.Sequential(
-            nn.Conv2d(3, 9, kernel_size=(3,3), padding=1, stride=(3, 3)),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(9, 64, kernel_size=(3,3), padding=1, stride=(1, 1)),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(64, 1, kernel_size=(3,3), padding=1, stride=(1, 1)),
-            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(3, 1, kernel_size=(5,5), padding=0, stride=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2)),
         )
+        self.linear = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(25347, 64*128),
+            nn.LeakyReLU(),
+            nn.Linear(64*128, 64*64 * 2),
+            nn.LogSoftmax(dim=1),
+        )
+        self.unflatten = nn.Unflatten(1, (2, 64, 64))
     def forward(self, x):
-        x = self.pooling(x)
+        # x = self.pooling(x)
         x = self.convolutional(x)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+        x = self.unflatten(x)
         return x
 
-
 async def eval(model):
-    data, labels = await getBatch(10)
-    img = torch.tensor(data).to(train_device)
-    # images = images.swapaxes(1, 3)
-    print(data.shape, labels.shape)
-        
-    x = model(img)
-    x = x.cpu().detach().numpy()
-    for i in range(data.shape[0]):
-        mask = x[i].swapaxes(0, 2)
-        print(mask.shape)
-        mask = cv2.resize(mask, (1280, 720))
-        img = data[i].swapaxes(0, 2)
-        
-        zeros = np.zeros_like(img)
-        zeros[:, :, 0] = mask[:, :, 0]
-        zeros[:, :, 1] = mask[:, :, 1]
-
-        print(mask.shape)
-        cv2.imshow("test", zeros)
-        cv2.waitKey(0)
-        cv2.imshow("test", img)
-        cv2.waitKey(0)
-
-async def eval2(model):
-    data, labels = await getBatch(10)
-    img = torch.tensor(data).to(train_device)
-    # images = images.swapaxes(1, 3)
-    print(data.shape, labels.shape)
-        
-    x = model(img)
-    x = x.cpu().detach().numpy()
-    for i in range(data.shape[0]):
-        mask = x[i].swapaxes(0, 2)
-        mask = cv2.resize(mask, (1280, 720))
-        compare = (mask[:, :, 0]).astype(np.float16)
-        img = data[i].swapaxes(0, 2)
-        max_road = np.max(x[i][0])
-        min_road = np.min(x[i][0])
-        max_not = np.max(x[i][1])
-        min_not = np.min(x[i][1])
-        normalized_road = (mask[:, :, 0] - min_road) / (max_road - min_road)
-        normalized_not = (mask[:, :, 1] - min_not) / (max_not - min_not)
-        # print(max_road, min_road, "test", np.max(normalized), np.min(normalized))
-        
-        zeros = np.zeros_like(img)
-        final_mask = np.clip(((2 *normalized_road - (normalized_not * 0.5))).astype(np.float16), 0, 1)
-        zeros[:, :, 0] = final_mask
-        zeros[:, :, 1] = final_mask
-        zeros[:, :, 2] = final_mask
-
-        subtracted = cv2.multiply(img, zeros)
-        cv2.imshow("test", img)
-        cv2.waitKey(1000)    
-        cv2.imshow("test", zeros)
-        cv2.waitKey(1000)
-        cv2.imshow("test", subtracted)
-        cv2.waitKey(1000)
-
-async def eval3(model):
-    data, labels = await getBatch(10, preprocessed=True, resolution=(60, 107))
+    # data, labels = await getBatch(10, preprocessed=True, resolution=(64, 64))
+    data, labels = np.load('./binaryDataset/inputs_0.npy'), np.load('./binaryDataset/labels_0.npy')
+    
     img = torch.tensor(data).to(train_device)
     # images = images.swapaxes(1, 3)
     # print(data.shape, labels.shape)
         
     x = model(img)
     x = x.cpu().detach().numpy()
-    for i in range(data.shape[0]):
+    for i in range(440):
         mask = x[i].swapaxes(0, 2)
+        print(mask.shape)
         mask = cv2.resize(mask, (1280, 720))
-        gt = labels[i][0]
-        # print(gt.shape)
+        gt = labels[i][1]
         gt = cv2.resize(gt, (1280, 720))
         img = data[i].swapaxes(0, 2)
-        max_road = np.max(x[i])
-        min_road = np.min(x[i])
-        normalized_road = (mask - min_road) / (max_road - min_road)
-        # print(max_road, min_road, "test", np.max(normalized), np.min(normalized))
+        max_road = np.max(x[i][:, :, 0])
+        min_road = np.min(x[i][:, :, 0])
+        print(np.max(mask), np.min(mask))
+        normalized = (mask - min_road) / (max_road - min_road)
+        print(max_road, min_road, "test", np.max(normalized), np.min(normalized))
+        compare = (mask[:, :, 0] >= mask[:, :, 1]).astype(np.float32)
         
         zeros = np.zeros_like(img)
-        # zeros[:, :, 1] = np.where(gt > normalized_road, 1, 0)
-        # zeros[:, :, 2] = normalized_road
-        # zeros[:, :, 1] = np.where(gt <= normalized_road, 1, 0)
+        zeros[:, :, 0] = compare
+        zeros[:, :, 1] = compare
+        zeros[:, :, 2] = compare
         # zeros[:, :, 2] = mask
 
-        subtracted = cv2.add(img, zeros)
+        added = cv2.multiply(img, zeros)
         cv2.imshow("test", img)
-        cv2.waitKey(1000)    
-        cv2.imshow("test", zeros)
-        cv2.waitKey(1000)
-        cv2.imshow("test", subtracted)
-        cv2.waitKey(1000)
+        cv2.waitKey(500)    
+        # cv2.imshow("test", zeros)
+        # cv2.waitKey(500)
+        # cv2.imshow("test", added)
+        # cv2.waitKey(500)
 
 async def train():
-    epoch = 7
-    batch_size = 1000
-    gpu_batch = 5
+    epoch = 100
+    gpu_batch = 4
     running_loss = 0
-    decay = 1/2
+    decay = 99/100
+    learning_rate = 0.001
     model = Segmentation().to(train_device)
-    # model.load_state_dict(torch.load("./models/segmentation_1.pth"))
-    loss_fn = nn.BCEWithLogitsLoss()
+    model.load_state_dict(torch.load("./models/segmentation_0.pth", weights_only=True))
+    loss_fn = nn.MSELoss()
     
-    images, labels = await getBatch(batch_size, preprocessed=True, resolution=(60, 107))
-    learning_rate = 0.1
+    images, labels = np.load('./binaryDataset/inputs_0.npy'), np.load('./binaryDataset/labels_0.npy')
+    batch_size = images.shape[0]
+    print("BATCH SIZE", batch_size)
     for i in range(epoch):
         learning_rate *= decay
-        print(f"=====EPOCH {i + 1}/{epoch}======  lr:", learning_rate)
+        if i % 10 == 0:
+            print(f"=====EPOCH {i + 1}/{epoch}======  lr:", learning_rate)
+            print("RUNNING_LOSS:", running_loss)
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         for j in range(gpu_batch):
             size = batch_size // gpu_batch
-            print(f"Batch {j + 1}/{batch_size}")
             end = min((size * j) + size, batch_size)
+            # print(f"Batch {j + 1}/{gpu_batch}", end)
             optimizer.zero_grad()
 
             data = torch.tensor(images[size * j: end]).to(train_device)
             target = torch.tensor(labels[size * j: end]).to(train_device)
-            print(data.shape, target.shape)
             
             outputs = model.forward(data)
             loss = loss_fn(outputs, target)
@@ -171,14 +122,13 @@ async def train():
             loss.backward()
 
             optimizer.step()
-        print("RUNNING_LOSS:", running_loss)
-    torch.save(model.state_dict(), f'./models/segmentation_1.pth')
-    await eval3(model)
+    torch.save(model.state_dict(), f'./models/segmentation_0.pth')
+    await eval(model)
 
 
 model = Segmentation().to(train_device)
-model.load_state_dict(torch.load("./models/segmentation_0.pth"))
-asyncio.run(eval3(model))
-# asyncio.run(eval(model))
+model.load_state_dict(torch.load("./models/segmentation_0.pth", weights_only=True))
+asyncio.run(eval(model))
+
 
 # asyncio.run(train())
